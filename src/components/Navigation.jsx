dac41da3c227330538ed7ac04d878d7cbfbc3eec
Westley Feller <feller.westley@gmail.com>
2019-10-02 18:39:07 -0700

    Linted

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navigation = () => (
+  <div>
+    <div className="App">
+      <h1>React Router Mini</h1>
+      <div>
+        <a href="">Home</a>
+      </div>
+      <div>
+        <a href="">About</a>
+      </div>
+      <div>
+        <a href="">Contact</a>
+      </div>
+    </div>
+  </div>
+);
+
+export default Navigation;
