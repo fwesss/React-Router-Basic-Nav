dcc16b0da9577dfc50e5aac5b4a5c3d16019a349
Westley Feller <feller.westley@gmail.com>
2019-10-02 19:06:28 -0700

    Finished MVP

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
+
 import {
   Home, About, Contact, Navigation,
 } from './components';
 
+import './App.css';
+
+
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/about" component={About} />
   </div>
 );
 
